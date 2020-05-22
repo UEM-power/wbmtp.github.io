6b06df8765cda4a18ba630d7272f18c1aa0a3dcc
UEM-power <64276544+UEM-power@users.noreply.github.com>
2020-05-23 03:08:08 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,4 +7,15 @@ $(function(){
   $('#signup-show').click(function(){
     $('#signup-modal').fadeIn();
   });
+
+  $('.close-modal').click(function(){
+    $('#login-modal').fadeOut();
+    $('#signup-modal').fadeOut();
+  });
+
+  $('.lesson-hover').hover(function(){
+    $(this).find('.text-contents').addClass('text-active');
+  },function(){
+    $(this).find('.text-contents').removeClass('text-active');
+  });
 });
